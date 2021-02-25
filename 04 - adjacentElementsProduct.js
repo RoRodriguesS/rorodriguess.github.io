4a0528ed9e3275bf7df4e3074c600d9696318aa7
devrosilva <rodrigosilva.info@gmail.com>
2021-02-25 01:29:13 -0300

    Exercise found and solved at Codecademy.com as part of the Front-End Engineer Career Path

--- 04 - adjacentElementsProduct.js
+++ 04 - adjacentElementsProduct.js
@@ -9,19 +9,21 @@ adjacentElementsProduct(inputArray) = 21.
 7 and 3 produce the largest product.
 */
 
+//Array created and populated just to facilitate test. To use your own values just edit or comment this line.
+let arr = [1,5,2,6,8,10];
+
 // My solution (Using Javascript)
 function adjacentElementsProduct(inputArray) {
     let i = 0;
     let auxVar = inputArray[i] * inputArray[i+1];
-    let auxArray = [0,0];
     
     while(i < inputArray.length){    
         if(inputArray[i] * inputArray[i+1] > auxVar){
-            auxArray[0] = inputArray[i];
-            auxArray[1] = inputArray[i+1];
             auxVar = inputArray[i] * inputArray[i+1];            
         }
         i++;
     }
     return auxVar;
 }
+//write the result at the page.
+document.write(adjacentElementsProduct(arr));
